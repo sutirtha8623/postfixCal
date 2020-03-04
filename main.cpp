6bb0227529b3892376be6cecc8b018a1ac456e64
sutirtha8623 <sutirtha8623@gmail.com>
2020-03-05 00:27:23 +0530

    file I/O

--- main.cpp
+++ main.cpp
@@ -2,20 +2,50 @@
 #include<string>
 #include<stdlib.h>
 #include"postfix_eval.h"
+#include<fstream>
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
 	string s;
-    while(true)
-    {
-    	cout<<"->";
-    	getline(cin, s);
-    	if(s!="exit" || s!="quit")
-    	{
-    		cout<<"->"<<eval_postfix(s)<<endl;
-    	}
-    	else
-    		exit(0);
-    }
+	if(argc==1)
+	{
+		cout<<"(enter expression/q to quit)\n";
+		while(true)
+		{
+			cout<<"->";
+			getline(cin, s);
+			if(s!="q")
+			{
+				cout<<"->"<<eval_postfix(s)<<"\n";
+			}
+			else
+				exit(0);
+		}
+	}
+
+	else if(argc==2)
+	{
+		string inp(argv[1]);
+		ifstream in_file(inp);
+		ofstream out_file("result.txt");
+		while(getline(in_file, s))
+		{
+			out_file<<eval_postfix(s)<<"\n";
+		}
+		cout<<"results have been written in results.txt\n";
+	}
+	else if(argc == 3)
+	{
+		string inp(argv[1]);
+		string outp(argv[2]);
+		ifstream in_file(inp);
+		ofstream out_file(outp);
+		while(getline(in_file, s))
+		{
+			out_file<<eval_postfix(s)<<"\n";
+		}
+		cout<<"results have been written in "<<outp<<"\n";
+	}
+
 }
